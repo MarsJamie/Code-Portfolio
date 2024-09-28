ac3eeb40877b54a0a2c9eb4e64d0adf8740b6b9b
Mars-Jamie <itzmjbeans@gmail.com>
2024-09-28 03:11:13 -0400

    Updates to Java Project 1

--- Java Programs and Files/proj1/proj1/src/main/java/com/proj1/Pizza.java
+++ Java Programs and Files/proj1/proj1/src/main/java/com/proj1/Pizza.java
@@ -1,7 +1,54 @@
 package com.proj1;
+import java.util.ArrayList;
 
-public class Main {
+    enum CrustType {
+        Plain,
+        Butter,
+        Garlic,
+        GarlicButter,
+        Cheese
+    }
+
+    enum SizeType{
+        Small,
+        Medium,
+        Large,
+        XLarge,
+        XXLarge,
+        Party
+    }
+
+
+    public class Pizza {
+
+        private CrustType crust;
+        private ArrayList<String> toppings;
+        private SizeType size;
+        
+        public Pizza(CrustType crust, ArrayList<String> toppings, SizeType size){
+            this.crust = crust;
+            this.toppings = toppings;
+            this.size = size;
+        }
+
+
     public static void main(String[] args) {
 
-    }
+        ArrayList<String> toppings = new ArrayList<String>();
+        toppings.add("Cheese");
+        toppings.add("Onion");
+        toppings.add("Green Pepper");
+        toppings.add("Ham");
+        toppings.add("Pineapple");
+        toppings.add("Pepperoni");
+        toppings.add("Ground Beef");
+        toppings.add("Italian Sausage");
+        toppings.add("Anchovies");
+
+        Pizza pizza = new Pizza(CrustType.Plain, toppings, SizeType.Small);
+
+     }
+    
 }
+
+
